753cbe4aad50a48cacba1f751e8f1fe0cb43e9d6
王立广 <wangliguang@daling.com>
2019-06-12 08:48:00 +0800

    feat: 添加日志和下载的总时间

--- ios/rn_phoenix/LoadBundleViewController.m
+++ ios/rn_phoenix/LoadBundleViewController.m
@@ -13,7 +13,7 @@
 #import "DiffMatchPatch.h"
 #import <Foundation/NSObject.h>
 @interface LoadBundleViewController ()
-
+@property (nonatomic, assign) NSTimeInterval currentTime;
 @property (weak, nonatomic) IBOutlet UILabel *bundleStatusText;
 
 @end
@@ -30,19 +30,23 @@
   if (!currentBundleVersion) {
     currentBundleVersion = @"0";
   }
+  NSLog(@"✅✅✅开始获取最新版本信息");
+  self.currentTime = [self currentTimeStr];
+  
   NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://47.94.81.19:3000/users/getPatch?currentBundleVersion=%@", currentBundleVersion]];
   [[session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
     if (error) {
-      NSLog(@"getPatch_error=%@", error);
+      NSLog(@"❌❌❌获取最新版本信息失败 =%@", error);
       return;
     }
     NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
-    NSLog(@"jsonDict %@", jsonDict);
     if(jsonDict[@"bundleUrl"]) {
+      NSLog(@"✅✅✅本地没有bundle，开始下载整个bundle包，%@", jsonDict);
       [self downLoadBundle:jsonDict[@"bundleUrl"] version:jsonDict[@"version"]];
     };
     
     if(jsonDict[@"patchUrl"]) {
+      NSLog(@"✅✅✅本地已有bundle，开始下载差分包，%@", jsonDict);
       [self downLoadPatch:jsonDict[@"patchUrl"] version:jsonDict[@"version"]];
     };
   }] resume];
@@ -53,30 +57,34 @@
   NSURL *url = [NSURL URLWithString:urlStr];
   NSURLSession *session = [NSURLSession sharedSession];
   [[session downloadTaskWithURL:url completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
-    
+    if (error) {
+      NSLog(@"❌❌❌差分包下载失败 =%@", error);
+      return;
+    }
     NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
     NSString *patchFilePath = [documentsPath stringByAppendingPathComponent:@"diff.pat"];
     [WHCFileManager moveItemAtPath:location.path toPath:patchFilePath overwrite:true];
+    NSLog(@"✅✅✅差分包下载完成，开始合并，path=%@", [self getBundlePath]);
     [self benginPatch:patchFilePath version:currentBundleVersion];
   }] resume];
 }
 
 - (void)benginPatch:(NSString *)patchesPath version: (NSString *)currentBundleVersion {
   NSString *path01 = [self getBundlePath];
-  NSLog(@"path %@", [self getBundlePath]);
   NSData *data01 = [NSData dataWithContentsOfFile:path01];
   NSString *str01 = [[NSString alloc] initWithData:data01 encoding:NSUTF8StringEncoding];
   DiffMatchPatch *patch = [[DiffMatchPatch alloc]init];
   NSData *patchesData = [NSData dataWithContentsOfFile:patchesPath];
-  NSLog(@"bundleFilePath====%@", [self getBundlePath] );
   NSString *patchesStr = [[NSString alloc]initWithData:patchesData encoding:NSUTF8StringEncoding];
   NSMutableArray *patchesArr = [patch patch_fromText:patchesStr error:nil];
   NSArray *newArray = [patch patch_apply:patchesArr toString:str01];
-  BOOL isTrue = [newArray[0] writeToFile:[self getBundlePath] atomically:YES encoding:NSUTF8StringEncoding error:nil];
+  NSError *error = nil;
+  BOOL isTrue = [newArray[0] writeToFile:[self getBundlePath] atomically:YES encoding:NSUTF8StringEncoding error:&error];
   if (!isTrue) {
-    NSLog(@"写入失败");
+    NSLog(@"❌❌❌差分包合并失败=%@", error);
     return;
   }
+  NSLog(@"✅✅✅差分包合并完成");
   [self mainQueueUpdateUI:currentBundleVersion];
 }
 
@@ -84,7 +92,12 @@
   NSURL *url = [NSURL URLWithString:urlStr];
   NSURLSession *session = [NSURLSession sharedSession];
   [[session downloadTaskWithURL:url completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
+    if (error) {
+      NSLog(@"❌❌❌bundle下载失败=%@", error);
+      return;
+    }
     [WHCFileManager moveItemAtPath:location.path toPath:[self getBundlePath] overwrite:true];
+    NSLog(@"✅✅✅bundle下载完毕 path=%@", [self getBundlePath]);
     [self mainQueueUpdateUI:currentBundleVersion];
   }] resume];
 }
@@ -95,6 +108,8 @@
     RCTBridge *bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation moduleProvider:nil launchOptions:nil];
     RCTRootView* view = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"rn_phoenix" initialProperties:nil];
     [[NSUserDefaults standardUserDefaults] setObject:currentBundleVersion forKey:CURRENT_BUNDLE_VERSION];
+    NSLog(@"✅✅✅bundle加载完毕，消耗的总时间为=%f", [self currentTimeStr] - self.currentTime);
+    
     self.view = view;
   });
 }
@@ -102,8 +117,17 @@
 - (NSString *)getBundlePath {
   NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
   NSString *bundleFilePath = [documentsPath stringByAppendingPathComponent:@"index.bundle"];
+  
+  [self currentTimeStr];
   return bundleFilePath;
 }
+
+- (NSTimeInterval)currentTimeStr{
+  NSDate* date = [NSDate dateWithTimeIntervalSinceNow:0];//获取当前时间0秒后的时间
+  NSTimeInterval time=[date timeIntervalSince1970]*1000;// *1000 是精确到毫秒，不乘就是精确到秒
+  return time;
+}
+
 
 
 @end
